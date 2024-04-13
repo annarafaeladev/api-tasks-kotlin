216c7df113b9b9b8dd6c6c5a4942666577906b67
Anna Rafaela Bispo de Santana <anna.santana@compasso.com.br>
2024-04-13 14:22:09 -0300

    refactor: create task

--- src/main/kotlin/com/br/api/api/service/TaskService.kt
+++ src/main/kotlin/com/br/api/api/service/TaskService.kt
@@ -1,6 +1,8 @@
 package com.br.api.api.service
 import com.br.api.api.dtos.TaskDto
 import com.br.api.api.entity.Task
+import com.br.api.api.enumeration.TaskExceptionType
+import com.br.api.api.exception.TaskException
 import com.br.api.api.repository.TaskRepository
 import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
@@ -8,10 +10,17 @@ import java.util.UUID
 
 @Service
 class TaskService(private val taskRepository: TaskRepository) {
+    fun createTask(taskDto: TaskDto): Task {
+        try {
+            return savedTask(taskDto)
+        } catch (ex: Exception) {
+            throw TaskException(TaskExceptionType.UNABLE_TO_CREATE_TASK, ex)
+        }
+    }
 
     @Transactional
-    fun saveTask(task: TaskDto): Task {
-        val newTask = Task(task.title, task.description, task.priority)
+    private fun savedTask(taskDto: TaskDto) : Task {
+        val newTask = Task(taskDto.title, taskDto.description, taskDto.priority)
         return taskRepository.save(newTask)
     }
 
